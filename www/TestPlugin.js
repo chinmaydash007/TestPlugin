888d6e37956b49a4915924adcfe4136fc93817a8
chinmay <chinmaydash11@gmail.com>
2023-07-10 17:59:39 +0530

    added missing .js file code

--- www/TestPlugin.js
+++ www/TestPlugin.js
@@ -1,5 +1,14 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'TestPlugin', 'coolMethod', [arg0]);
+  exec(success, error, "TestPlugin", "coolMethod", [arg0]);
 };
+
+var TestPlugin = function () {}; // This just makes it easier for us to export all of the functions at once.
+// All of your plugin functions go below this.
+// Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
+
+TestPlugin.addTwoNumber = function (arg0, onSuccess, onError) {
+  exec(onSuccess, onError, "TestPlugin", "addTwoNumber", [arg0]);
+};
+module.exports = TestPlugin;
