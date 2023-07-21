a2f5338bb1d39def8c9b14d05b12f02e46c6fb7f
Yash <yash-vardhan@hotmail.com>
2023-07-21 17:11:22 +0530

    added addTwoNumber in test plugin

--- www/TestPlugin.js
+++ www/TestPlugin.js
@@ -3,7 +3,3 @@ var exec = require("cordova/exec");
 exports.coolMethod = function (arg0, success, error) {
   exec(success, error, "TestPlugin", "coolMethod", [arg0]);
 };
-
-exports.addTwoNumber = function (arg0, onSuccess, onError) {
-  exec(onSuccess, onError, "TestPlugin", "addTwoNumber", [arg0]);
-}
