af615c6fb8853f24748fe04fdd30f0f10575a283
Yash <yash-vardhan@hotmail.com>
2023-07-21 16:34:29 +0530

    added addTwoNumber in test plugin

--- www/TestPlugin.js
+++ www/TestPlugin.js
@@ -4,11 +4,6 @@ exports.coolMethod = function (arg0, success, error) {
   exec(success, error, "TestPlugin", "coolMethod", [arg0]);
 };
 
-var TestPlugin = function () {}; // This just makes it easier for us to export all of the functions at once.
-// All of your plugin functions go below this.
-// Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-
-TestPlugin.addTwoNumber = function (arg0, onSuccess, onError) {
+exports.addTwoNumber = function (arg0, onSuccess, onError) {
   exec(onSuccess, onError, "TestPlugin", "addTwoNumber", [arg0]);
-};
-module.exports = TestPlugin;
+}
